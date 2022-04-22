572880e5f92d6f2288819c1ab4756170f583060d
Akash Nainani <akashnainani9@gmail.com>
2022-04-21 22:36:40 -0400

    Login css fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,53 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonItemDivider,IonIcon, IonButton, IonTabBar, IonTabButton } from '@ionic/react';
-import { calendar, people, informationCircle, home, book, albums,logoGoogle } from 'ionicons/icons';
-import { useLocation } from 'react-router-dom';
-import { useHistory } from 'react-router';
-import { getAuth, signInWithEmailAndPassword,signInWithPopup,GoogleAuthProvider } from "firebase/auth";
-import { useStore } from "../App"
-import { appAuth} from "../Firebase"
+import React, { useEffect, useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonItemDivider,
+  IonIcon,
+  IonButton,
+  IonTabBar,
+  IonTabButton,
+  IonGrid,
+  IonRow,
+} from "@ionic/react";
+import { logoGoogle } from "ionicons/icons";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { useStore } from "../App";
+import { appAuth } from "../Firebase";
 
 const Auth = getAuth(appAuth);
 
 const Login: React.FC = () => {
-  const {setSignedIn, setPortfolio, setUid, setName} = useStore();
+  const { setSignedIn, setPortfolio, setUid, setName } = useStore();
   const [username, setUsername] = useState<string>();
   const [password, setPasword] = useState<string>();
 
-  const login=()=>{
+  const login = () => {
     signInWithEmailAndPassword(Auth, username!, password!)
-  .then(userCredential => {
-    console.log(userCredential);
-    setSignedIn(true)
-    const user = userCredential.user;
-    console.log(user)
-    setPortfolio(user.uid)
-    setTimeout(()=>{window.location.href = "/home?q=Software companies"},400)
-    
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-  }
+      .then((userCredential) => {
+        console.log(userCredential);
+        setSignedIn(true);
+        const user = userCredential.user;
+        console.log(user);
+        setPortfolio(user.uid);
+        setTimeout(() => {
+          window.location.href = "/home?q=Software companies";
+        }, 400);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
 
-  const googleLogin=()=>{
-    signInWithPopup(Auth,new GoogleAuthProvider())
-    .then(userCredential => {
-      console.log(userCredential);
-      setSignedIn(true)
-      const user = userCredential.user;
-      console.log(user.uid)
-      setName(String(user.displayName))
-      setUid(user.uid)
-      setPortfolio(user.uid)
-      setTimeout(()=>{window.location.href = "/home?q=Software companies"},400)
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-  }
+  const googleLogin = () => {
+    signInWithPopup(Auth, new GoogleAuthProvider())
+      .then((userCredential) => {
+        console.log(userCredential);
+        setSignedIn(true);
+        const user = userCredential.user;
+        console.log(user.uid);
+        setName(String(user.displayName));
+        setUid(user.uid);
+        setPortfolio(user.uid);
+        setTimeout(() => {
+          window.location.href = "/home?q=Software companies";
+        }, 400);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
 
   return (
     <IonPage>
@@ -57,26 +80,44 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        { (useStore.getState().signedInVal === false || useStore.getState().signedInVal === null) &&
-        <IonList>
-          {/* <IonItemDivider>Default Input with Placeholder</IonItemDivider> */}
-          <IonItem>
-            <IonInput value={username} placeholder="Enter Username" onIonChange={e => setUsername(e.detail.value!)}></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonInput value={password} placeholder="Enter Password" type="password" onIonChange={e => setPasword(e.detail.value!)}></IonInput>
-          </IonItem>
-        <IonButton onClick={login}>Submit</IonButton>
-        
-        </IonList>
-        
-        }
-        <IonIcon icon={logoGoogle} onClick={googleLogin} size="large"/>
-        {(useStore.getState().signedInVal === true) && <p>Congrats You Have Successfully Signed In!</p>}
+        {(useStore.getState().signedInVal === false ||
+          useStore.getState().signedInVal === null) && (
+          <IonList>
+            {/* <IonItemDivider>Default Input with Placeholder</IonItemDivider> */}
+            <IonItem>
+              <IonInput
+                value={username}
+                placeholder="Enter Username"
+                onIonChange={(e) => setUsername(e.detail.value!)}
+              ></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonInput
+                value={password}
+                placeholder="Enter Password"
+                type="password"
+                onIonChange={(e) => setPasword(e.detail.value!)}
+              ></IonInput>
+            </IonItem>
+            <IonButton onClick={login}>Submit</IonButton>
+          </IonList>
+        )}
+        <IonGrid>
+          <IonRow class="ion-justify-content-center">
+            -----------------------------------------------
+          </IonRow>
+          <IonRow class="ion-justify-content-center">
+            <IonButton onClick={googleLogin} fill="outline">
+              <IonIcon icon={logoGoogle} size="large" />
+            </IonButton>
+          </IonRow>
+        </IonGrid>
+        {useStore.getState().signedInVal === true && (
+          <p>Congrats You Have Successfully Signed In!</p>
+        )}
       </IonContent>
-    </IonPage >
+    </IonPage>
   );
 };
-
 
 export default Login;
